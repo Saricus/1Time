f06c17a10c560adffe9598e5609f1117a3fc7d36
Saricus <saricuss@gmail.com>
2024-01-24 08:36:40 +0100

    Initial commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+name = input('Podaj Nick: ')
+if name.isalnum():
+    print('Brawo nick poprawny')
+else:
+    print('Podaj nick bez znaków specjalnich')
---
+++ 2.py
@@ -0,0 +1,20 @@
+a = int(input('Podaj 1 cyfre: '))
+print('Podaj wybierz znak')
+print('1. +')
+print('2. -')
+print('3. *')
+print('4. /')
+b = int(input('Jaki znak?: '))
+c = int(input('Podaj 2 cyfre: '))
+
+if b == 1:
+    print(a + c)
+elif b == 2:
+    print(a - c)
+elif b == 3:
+    print(a * c)
+elif b == 4:
+    print(a / c)
+else:
+    print('Niepoprawny wybór znaku wybierz numer 1-4')
+
