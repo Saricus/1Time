50137dd46e1be45f899932d4a3e5a7fe94ecc363
Saricus <saricuss@gmail.com>
2024-01-24 20:33:28 +0100

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,7 @@
+while True:
+    num = int(input('Podaj liczbę wieksza od 5: '))
+    if num > 5:
+        print('Towja liczba to:', num)
+        break
+    else:
+        print('Twoja liczba jest mniejsza od 5')
