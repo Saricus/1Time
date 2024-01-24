ac567c4323de9892757cd55ca0b933c269662605
Saricus <saricuss@gmail.com>
2024-01-24 20:39:29 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 name = input('Podaj Nick: ')
 
-if len(name) < 3:
+if len(name) < 3: # Jeśli mniejsze od 3
     print('Nick musi zawierać co najmniej 3 znaki')
-elif name.isalnum():
+elif name.isalnum(): # Jeśli jest literą lub cyfrą
     print('Brawo nick poprawny')
 else:
     print('Podaj nick bez znaków specjalnich')
