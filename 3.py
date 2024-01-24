760b1aa7dcc787ca1cb43a98979ab0bfd0756536
Saricus <saricuss@gmail.com>
2024-01-24 18:52:48 +0100

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+print('Maxymalna pojemnośc magazynu to 100 miejsc')
+max = 100
+psc = int(input('Podaj ilość w magazynie: '))
+dostawa = int(input('Podaj ilość w dostawei: '))
+razem = psc + dostawa
+wolne = max - (psc + dostawa)
+if psc + dostawa > max:
+    print('Magazyn nie pomieści tyle towaru')
+else:
+    print('Można pakowac. W magazynie pozostanie {} miejsc'.format(wolne))
