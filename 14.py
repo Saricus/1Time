97f0e2a58265b00efab96bfba3c839d579ab1b9d
Saricus <saricuss@gmail.com>
2024-01-25 18:08:53 +0100

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,5 @@
+lista = [10, 4, 55, 23, 20, 30, 40]
+krotka = tuple(lista) # tworzy typle z listy
+print(max(krotka)) #jaka jest najwieksza wartosc
+print(min(krotka)) #jaka jest najmniejsza wartosc
+print(sum(krotka)) #jaka jest suma wszystkich wartosci
