e49cab7ac866a92445b6cfe81880aea828ec894d
Saricus <saricuss@gmail.com>
2024-01-25 19:38:46 +0100

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,14 @@
+# Funkcje
+# bloki kodu wielokrotnego wykorzystania
+# zestaw deklaracji ktore czesto sie powtarzaja
+# kluczowy element jezyka
+# liczne funkcje wbudowane oraz mozliwosc definiowania wlasnych
+
+# def nazwa_funkcji(argument):
+    # deklaracje
+
+
+def kappa():
+    print('2137')
+
+kappa()
