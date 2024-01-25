4617b986a90c2cf0f652caabd9a4c1aace01f041
Saricus <saricuss@gmail.com>
2024-01-25 18:08:49 +0100

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,4 +1,4 @@
 #Tuples
-krotka = (1, 2, 3, 4)
-#krotka[0] = 20   -- do Tuple nie można dodawać danych
+krotka = (1, 2, 3, 4, True, 'abc')
+#krotka[0] = 20   -- do Tuple nie da się dodawać danych
 print(krotka[1])
