02585dea69726237927472b98b53d41706a628e5
Saricus <saricuss@gmail.com>
2024-01-25 18:08:51 +0100

    Update 13.py

---
+++ 13.py
@@ -0,0 +1,5 @@
+krotka = (1, 2, 3, 4, 5, 6, 7)
+nowa_krotka = krotka[:3]
+print(2 in nowa_krotka)
+print(nowa_krotka + (10, 20)) #dodanie nowego tuple 
+print(len(nowa_krotka + (10, 20))) #dlugosc tuple
