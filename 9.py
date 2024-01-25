e217d46e42df2db3869f6a822362afa080376293
Saricus <saricuss@gmail.com>
2024-01-25 17:51:13 +0100

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,8 @@
+lista = [10, 20, 30, 40]
+lista.append(53) #dodaje na koniec listy
+lista.append(12)
+lista.append(8)
+lista.sort() # sortuje liste
+lista.reverse() # obraca liste
+for v in lista:
+    print(v)
